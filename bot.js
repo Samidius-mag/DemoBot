fa729c7317cc7d2e43a5046151cccf322446ff11
Samidius (VoroniN) <78709877+Samidius-mag@users.noreply.github.com>
2023-05-19 11:28:42 +1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api');
 const fs = require('fs');
 
-// Токен бота, полученный от BotFather
+// Токен бота, который вы получили у BotFather
 const token = '6237100701:AAFDTCeZw8wWGc6MQw1oBvea6Nk-zKrV3t4';
 
 // Создаем экземпляр бота
@@ -10,14 +10,31 @@ const bot = new TelegramBot(token, { polling: true });
 // Обработчик команды /start
 bot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id;
-  const username = msg.chat.username;
 
-  // Добавляем пользователя в файл users.json
-  const user = { id: chatId, username: username };
-  const users = JSON.parse(fs.readFileSync('users.json', 'utf8'));
-  users.push(user);
-  fs.writeFileSync('users.json', JSON.stringify(users));
+  // Сохраняем ид пользователя в файл users.json
+  fs.readFile('users.json', (err, data) => {
+    let users = [];
 
-  // Отправляем сообщение пользователю
-  bot.sendMessage(chatId, 'Вы были добавлены в список пользователей.');
+    if (!err) {
+      users = JSON.parse(data);
+    }
+
+    users.push(chatId);
+
+    fs.writeFile('users.json', JSON.stringify(users), (err) => {
+      if (err) {
+        console.error(err);
+        bot.sendMessage(chatId, 'Произошла ошибка при сохранении ид пользователя');
+      } else {
+        bot.sendMessage(chatId, 'Ваш ид успешно сохранен');
+      }
+    });
+  });
 });
+
+// Запускаем бота
+bot.on('polling_error', (error) => {
+  console.error(error);
+});
+
+console.log('Бот запущен');
